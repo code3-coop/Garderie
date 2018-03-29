7edeb4ba6f4e29851507b2591c81641abd144a50
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-03-29 16:49:06 -0400

    add simple seed of data

---
+++ seed.sql
@@ -0,0 +1,3 @@
+insert into "group" (id, name, educator) values (1, 'Chouchoux-doux', 'Sylvie');
+insert into parents (id, parent_1_name, parent_2_name, parent_1_phone, parent_2_phone) values (1, 'Alain lalonde', 'Rana Likouma', '514-210-4333', '438-099-1234');
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (1, 'Sylvain', 'Chose', '2017-09-01', null, 1, 1);
