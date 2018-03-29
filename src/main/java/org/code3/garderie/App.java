96a6369ad7173140da6998818f2c298d6b122190
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-03-29 16:49:32 -0400

    more clean up

--- src/main/java/org/code3/garderie/App.java
+++ src/main/java/org/code3/garderie/App.java
@@ -3,12 +3,11 @@ package org.code3.garderie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-// import org.springframework.context.annotation.ComponentScan;
-
 
 @EnableAutoConfiguration
 @SpringBootApplication
 public class App {
+
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
   }
