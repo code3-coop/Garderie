e84a19dbee408ea4d9f10a0c5c73943fdd4e0149
Pascale Bourassa <pascale.bourassa@code3.ca>
2018-04-06 11:32:54 -0400

    Avoir un peu plus de données

--- seed.sql
+++ seed.sql
@@ -1,3 +1,7 @@
 insert into "group" (id, name, educator) values (1, 'Chouchoux-doux', 'Sylvie');
 insert into parents (id, parent_1_name, parent_2_name, parent_1_phone, parent_2_phone) values (1, 'Alain lalonde', 'Rana Likouma', '514-210-4333', '438-099-1234');
-insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (1, 'Sylvain', 'Chose', '2017-09-01', null, 1, 1);
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (1, 'William', 'Chose', '2017-09-01', null, 1, 1);
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (2, 'Mathéo', 'Boyer', '2016-09-12', null, 1, 1);
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (3, 'Kyle', 'Boc', '2015-12-12', null, 1, 1);
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (4, 'Joshua', 'Boc', '2014-08-12', null, 1, 1);
+insert into child (id, firstname, lastname, birthdate, image_url, parents, "group") values (5, 'Clara', 'Dreux', '2016-03-22', null, 1, 1);
