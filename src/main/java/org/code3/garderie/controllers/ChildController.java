f94cd3d4ee257002619a814dde699000e65c0f40
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-04-13 08:03:48 -0400

    add logging to childController

--- src/main/java/org/code3/garderie/controllers/ChildController.java
+++ src/main/java/org/code3/garderie/controllers/ChildController.java
@@ -1,11 +1,13 @@
 package org.code3.garderie;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.stereotype.Controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.Date;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ChildController {
@@ -13,8 +15,11 @@ public class ChildController {
   @Autowired
   ChildRepository childRepository;
 
+  private static final Logger log = LoggerFactory.getLogger(ChildController.class);
+
   @GetMapping("/child/list")
   public String list(ModelMap model){
+    log.debug("list ");
     var group = new Group(1l, "", "");
     var childs = childRepository.getChildrenByGroup(group);
     model.addAttribute("children", childRepository.getAllChildren());
