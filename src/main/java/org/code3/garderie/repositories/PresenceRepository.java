8bf921617507497bd46cdcc3e71c018aea7806bc
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-04-13 08:04:46 -0400

    add method to get list of presence for a child by it's id

--- src/main/java/org/code3/garderie/repositories/PresenceRepository.java
+++ src/main/java/org/code3/garderie/repositories/PresenceRepository.java
@@ -12,9 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class PresenceRepository {
   private static final Logger log = LoggerFactory.getLogger(PresenceRepository.class);
-  private static final String GET_PRESENCE_BY_CHILD_AND_DATE = "select date, state, child, absence_reason, author from presence where child = ? and date = ?";
-  private static final String CREATE_PRESENCE = "insert into presence (date, state, child, absence_reason, author) values (?, ?, ?, ?, ?);";
-  private static final String GET_PRESENCE_BY_CHILD = "select date, state, child, absence_reason, author from presence where child = ?";
+  private static final String GET_PRESENCE_BY_CHILD_AND_DATE = "" +
+    "select date, state, child, absence_reason, author from presence " +
+    "where child = ? and date = ?";
+  private static final String CREATE_PRESENCE = "" +
+    "insert into presence (date, state, child, absence_reason, author)" +
+    " values (?, ?, ?, ?, ?);";
+  private static final String GET_PRESENCE_BY_CHILD = "" +
+    "select date, state, child, absence_reason, author " +
+    " from presence where child = ?";
   private static final String GET_PRESENCE_BY_DATE_AND_GROUP = "" +
     "select p.date, p.state, p.child, p.absence_reason, p.author " +
     "from presence p " +
@@ -30,9 +36,15 @@ public class PresenceRepository {
     return jdbcTemplate.query(GET_PRESENCE_BY_DATE_AND_GROUP, new Object[] {date, group.getId()}, presenceRowMapper());
   }
 
+  //should return an id
   public void createPresence(Presence presence){
     log.debug("createPresence " + presence.toString());
     jdbcTemplate.update(CREATE_PRESENCE, presence.getDate(), presence.getState(), presence.getChild(), presence.getAbsenceReason(), presence.getAuthor());
+  }
+
+  public List<Presence> getPresenceByChild(Child child){
+    log.debug("getPresenceByChild " + child.toString());
+    return jdbcTemplate.query(GET_PRESENCE_BY_CHILD, new Object[] {child.getId()}, presenceRowMapper());
   }
 
   private RowMapper<Presence> presenceRowMapper(){
