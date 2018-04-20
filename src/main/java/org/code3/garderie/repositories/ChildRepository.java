439fb59889885488c9b9d41557edcc7963865596
Benjamin Dreux <benjamin.dreux@code3.ca>
2018-04-20 07:37:31 -0400

    use named parameterized jdbc in child repo

--- src/main/java/org/code3/garderie/repositories/ChildRepository.java
+++ src/main/java/org/code3/garderie/repositories/ChildRepository.java
@@ -3,44 +3,105 @@ package org.code3.garderie;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.Map;
 
 @Service
 public class ChildRepository {
 
   private static final Logger log = LoggerFactory.getLogger(ChildRepository.class);
-  private static final String GET_CHILD = "select id, firstname, lastname, birthdate, image_url, parents, \"group\" from child where id = ?;";
-  private static final String CREATE_CHILD = "insert into child (firstname, lastname, birthdate, image_url, parents, \"group\") values (?, ?, ?, ?, ?, ?);";
-  private static final String LIST_CHILD = "select id, firstname, lastname, birthdate, image_url, parents, \"group\" from child;";
-  private static final String GET_CHILDREN_BY_GROUP = "select id, firstname, lastname, birthdate, image_url, parents, \"group\" from child where \"group\" = ?;";
+  private static final String GET_CHILD = "" +
+    "select "  +
+    " id, "  +
+    " firstname, "  +
+    " lastname, "  +
+    " birthdate, "  +
+    " image_url, "  +
+    " parents, "  +
+    " \"group\" "  +
+    "from child "  +
+    " where id = :child_id;";
+
+  private static final String CREATE_CHILD = "" +
+    "insert into child (" +
+    " firstname, " +
+    " lastname, " +
+    " birthdate, " +
+    " image_url, " +
+    " parents, " +
+    " \"group\"" +
+    ") values (" +
+    " :firstname, " +
+    " :lastname, " +
+    " :birthdate, " +
+    " :image_url, " +
+    " :parents, " +
+    " :group" +
+    ");";
+
+  private static final String LIST_CHILD = ""+
+    "select "+
+    " id, "+
+    " firstname, "+
+    " lastname, "+
+    " birthdate, "+
+    " image_url, "+
+    " parents, "+
+    " \"group\" "+
+    "from child;";
+
+  private static final String GET_CHILDREN_BY_GROUP = ""+
+  "select "+
+  " id, "+
+  " firstname, "+
+  " lastname, "+
+  " birthdate, "+
+  " image_url, "+
+  " parents, "+
+  " \"group\" "+
+  "from child where \"group\" = :group_id;";
 
   @Autowired
-  JdbcTemplate jdbcTemplate;
+  NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
   public Child getChildById(long childId) {
     log.debug("Get child by id: " + childId);
-    return jdbcTemplate.queryForObject(GET_CHILD, new Object[] {childId}, childRowMapper());
+    Map<String, Long> params = Map.of(
+      "child_id", childId
+    );
+    return namedParameterJdbcTemplate.query(GET_CHILD, params, childRowMapper()).get(0);
   }
 
   @Transactional
   public void createChild(Child child){
     log.debug("Create child "+ child.toString());
-    jdbcTemplate.update(CREATE_CHILD, child.getFirstname(), child.getLastname(), child.getBirthdate(), child.getImageUrl(), child.getParents(), child.getGroup());
+    Map<String, Object> params = Map.of(
+      "firstname", child.getFirstname(),
+      "lastname", child.getLastname(),
+      "birthdate", child.getBirthdate(),
+      "image_url", child.getImageUrl(),
+      "parents", child.getParents(),
+      "group", child.getGroup()
+    );
+    namedParameterJdbcTemplate.update(CREATE_CHILD, params);
   }//XXX This should return a complete child (with Id)
 
   public List<Child> getAllChildren(){
     log.debug("Get list of child");
-    return jdbcTemplate.query(LIST_CHILD,childRowMapper());
+    return namedParameterJdbcTemplate.query(LIST_CHILD,childRowMapper());
   }
 
   public List<Child> getChildrenByGroup(Group group){
     log.debug("getChildrenByGroup" + group);
-    return jdbcTemplate.query(GET_CHILDREN_BY_GROUP, new Object[]{group.getId()}, childRowMapper());
+    Map<String, Object> params = Map.of(
+      "group_id", group.getId()
+    );
+    return namedParameterJdbcTemplate.query(GET_CHILDREN_BY_GROUP, params, childRowMapper());
   }
 
   private RowMapper<Child> childRowMapper(){
@@ -54,4 +115,13 @@ public class ChildRepository {
       rs.getLong("group")
     );
   }
+  // private class ChildRow {
+  //   Long id;
+  //   String firstname;
+  //   String lastname;
+  //   Date birthdate;
+  //   String image_url;
+  //   Long parents;
+  //   Long group;
+  // };
 }
